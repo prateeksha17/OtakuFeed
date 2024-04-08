68d69ecfaaf72cb3ef48ed6234a72e12983241d4
prateeksha17 <112319597+prateeksha17@users.noreply.github.com>
2024-04-08 22:47:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js'
-const path = require("path")
+
 const app = express()
 dotenv.config()
 
@@ -26,8 +26,3 @@ mongoose.connect(process.env.CONNECTION_URL)
 
 .catch(err => console.log(err.message))
 
-//productionscript
-app.use(express.static("./client/build"))
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
