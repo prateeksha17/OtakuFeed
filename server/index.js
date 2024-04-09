7a863cbbf55597b28449fef57236d1aa2d81c74d
prateeksha17 <112319597+prateeksha17@users.noreply.github.com>
2024-04-09 22:39:49 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,31 +4,27 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
-import userRoutes from './routes/users.js'
+import userRoutes from './routes/users.js';
 
-const app = express()
-dotenv.config()
-app.use(cors(
-  {
-    origin: ["https://otaku-feed-front.vercel.app"],
-             
-    methods: ["POST", "GET"],
-    credentials: true
-  }
-app.use(bodyParser.json({limit: "32mb", extended:true}))
-app.use(bodyParser.urlencoded({limit: "32mb", extended:true}))
-app.use(cors())
+const app = express();
+dotenv.config();
 
-app.use('/posts', postRoutes)
-app.use('/user', userRoutes)
+// Configuring CORS middleware
+app.use(cors({
+  origin: ["https://otaku-feed-front.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 
-const CONNECTION_URL = 'mongodb+srv://prateekshapandey8177:everything_sucks@cluster0.oj4pf3v.mongodb.net/?retryWrites=true&w=majority'
+app.use(bodyParser.json({ limit: "32mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "32mb", extended: true }));
 
+app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
-const PORT = process.env.PORT || 5000
+const CONNECTION_URL = 'mongodb+srv://prateekshapandey8177:everything_sucks@cluster0.oj4pf3v.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL)
-.then(()=> app.listen(PORT, ()=> console.log(`Server running on port :${PORT}`)))
-
-.catch(err => console.log(err.message))
-
+mongoose.connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch(err => console.log(err.message));
