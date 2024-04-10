ba2951819d447108ca25998f083dba08367d6042
prateeksha17 <112319597+prateeksha17@users.noreply.github.com>
2024-04-10 09:59:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -36,3 +36,5 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch(err => console.log(err.message));
+
+export default app;
