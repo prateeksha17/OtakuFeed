63e8720b092e3280e5aa27c681729a09ce7fd6fc
prateeksha17 <112319597+prateeksha17@users.noreply.github.com>
2024-04-10 09:47:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-
+import cors from 'cors;
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
-const cors = require('cors');
+
 const app = express();
 dotenv.config();
 
