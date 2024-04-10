3392004b1e785f4123a43e130412bf69d640e75e
prateeksha17 <112319597+prateeksha17@users.noreply.github.com>
2024-04-10 09:55:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import cors from 'cors;
+import cors from 'cors';
+
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
